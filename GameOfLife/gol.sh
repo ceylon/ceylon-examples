813e302f81e8563ccd16ef670483c1f33ce0f680
Enrique Zamudio <alterbass@gmail.com>
2013-02-27 10:51:45 -0600

    Compile swing subpackage and run with new runner

--- GameOfLife/gol.sh
+++ GameOfLife/gol.sh
@@ -1,6 +1,6 @@
 echo Compiling
-$CEYLON_HOME/bin/ceylon compile source/ceylon/examples/gameoflife/*.ceylon source/test/examples/gameoflife/*.ceylon
+$CEYLON_HOME/bin/ceylon compile source/ceylon/examples/gameoflife/*.ceylon source/test/examples/gameoflife/*.ceylon source/ceylon/examples/gameoflife/swing/*.ceylon
 echo Running tests
 $CEYLON_HOME/bin/ceylon run test.examples.gameoflife/1.0.0
 echo Running Game of Life
-$CEYLON_HOME/bin/ceylon run ceylon.examples.gameoflife/1.0.0
+$CEYLON_HOME/bin/ceylon run ceylon.examples.gameoflife/1.0.0 --run=ceylon.examples.gameoflife.swing.run
