01968d82d1e25b2520382d7bc308aa95f2047fba
Enrique Zamudio <alterbass@gmail.com>
2013-02-27 12:42:05 -0600

    Compile to js after compiling to jvm

--- GameOfLife/gol.sh
+++ GameOfLife/gol.sh
@@ -1,5 +1,6 @@
 echo Compiling
 $CEYLON_HOME/bin/ceylon compile source/ceylon/examples/gameoflife/*.ceylon source/test/examples/gameoflife/*.ceylon source/ceylon/examples/gameoflife/swing/*.ceylon
+$CEYLON_HOME/bin/ceylon compile-js source/ceylon/examples/gameoflife/*.ceylon source/test/examples/gameoflife/*.ceylon source/ceylon/examples/gameoflife/web/*.ceylon
 echo Running tests
 $CEYLON_HOME/bin/ceylon run test.examples.gameoflife/1.0.0
 echo Running Game of Life
